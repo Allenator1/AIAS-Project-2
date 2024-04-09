d72d614f86abee7be8b5660ad899e9d0e722d382
Allen <allenantony2001@gmail.com>
2024-04-10 02:16:25 +0800

    feat: incorporate Camo_Worm and Drawing class from CoCalc

---
+++ util.py
@@ -0,0 +1,52 @@
+import numpy as np
+import imageio.v3 as iio
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from sklearn.metrics.pairwise import euclidean_distances
+
+
+class Drawing:
+    def __init__ (self, image):
+        self.fig, self.ax = plt.subplots()
+        self.image = image
+        self.im = self.ax.imshow(self.image, cmap='gray', origin='lower')
+
+    def add_patches(self, patches):
+        try:
+            for patch in patches:
+                self.ax.add_patch(patch)
+        except TypeError:
+            self.ax.add_patch(patches)
+
+    def add_dots(self, points, radius=4, **kwargs):
+        try:
+            for point in points:
+                self.ax.add_patch(mpatches.Circle((point[0],point[1]), radius, **kwargs))
+        except TypeError:
+            self.ax.add_patch(mpatches.Circle((points[0],points[1]), radius, **kwargs))
+
+    def add_worms(self, worms):
+        try:
+            self.add_patches([w.patch() for w in worms])
+        except TypeError:
+            self.add_patches([worms.patch()])
+
+    def show(self, save=None):
+        if save is not None:
+            plt.savefig(save)
+        plt.show()
+        
+
+def crop (image, mask):
+    h, w = np.shape(image)
+    return image[max(mask[0],0):min(mask[1],h), max(mask[2],0):min(mask[3],w)]
+
+
+def prep_image (imdir, imname, mask):
+    print("Image name (shape) (intensity max, min, mean, std)\n")
+    image = np.flipud(crop(iio.imread(imdir+'/'+imname+".png"), mask))
+    print("{} {} ({}, {}, {}, {})".format(imname, np.shape(image), np.max(image), np.min(image), round(np.mean(image),1), round(np.std(image),1)))
+    plt.imshow(image, vmin=0, vmax=255, cmap='gray', origin='lower') # use vmin and vmax to stop imshow from scaling
+    plt.show()
+    return image
