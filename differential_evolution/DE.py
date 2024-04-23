d358523251b16c75eb1ad2b01ee21efc70a1fab5
Allen <allenantony2001@gmail.com>
2024-04-23 17:30:08 +0800

    feat: implement  a 'clew' class to store the Worm objects as well as a vector of parameters to be optimised. Adjust DE to use objects containing vectors.

--- differential_evolution/DE.py
+++ differential_evolution/DE.py
@@ -1,19 +1,32 @@
 import numpy as np
 
 
+class Target:
+	"""
+	Represents an individual within the population in DE
+	"""
+	def __init__(self, bounds, vector=None):
+		self.bounds = bounds
+		if vector is not None:
+			self.vector = vector
+		else:
+			self.vector = np.random.uniform(bounds[:, 0], bounds[:, 1])
+
+
 class DE:
 	"""
     Original Differential Evolution algorithm.
     """
-	def __init__(self, objective_function, bounds, NP=10, max_iter=1000, F=0.5, CR=0.9):
+	def __init__(self, objective_function, bounds, 
+			  initial_population, max_iter=1000, F=0.5, CR=0.9):
 		self.objective_function = objective_function
 		self.bounds = bounds
-		self.NP = NP
 		self.max_iter = max_iter
 		self.F = F
 		self.CR = CR
-		self.population = [np.random.uniform(bounds[:, 0], bounds[:, 1]) for _ in range(NP)]
 		self.generation = 0
+		self.population = initial_population
+		self.NP = len(initial_population)
 
 
 	def iterate(self):
@@ -22,16 +35,18 @@ class DE:
 			# Select three random indices
 			r1, r2, r3 = np.random.choice(self.NP, 3, replace=False)
 			target = self.population[i]
+			target_vector = target.vector
 
 			# Mutation
-			mutant = self.population[r1] + self.F * (self.population[r2] - self.population[r3])
+			mutant_vector = self.population[r1].vector + self.F * (self.population[r2].vector - self.population[r3].vector)
 
 			# Crossover
-			cross_points = np.random.rand(len(mutant)) < self.CR
-			trial = np.where(cross_points, mutant, target)
+			cross_points = np.random.rand(len(mutant_vector)) < self.CR
+			trial_vector = np.where(cross_points, mutant_vector, target_vector)
 
 			# Clip to bounds
-			trial = np.clip(trial, self.bounds[:, 0], self.bounds[:, 1])
+			trial_vector = np.clip(trial_vector, self.bounds[:, 0], self.bounds[:, 1])
+			trial = self.population[i].__class__(self.bounds, trial_vector)
 
 			# Selection
 			if self.objective_function(trial) < self.objective_function(target):
@@ -41,18 +56,20 @@ class DE:
 	
 	def get_best(self):
 		"""Return the index of the best individual in the population."""
-		return np.argmin([self.objective_function(x) for x in self.population])
+		return self.population[np.argmin([self.objective_function(x) for x in self.population])]
 	
 
 # Test usage
 if __name__ == '__main__':
 	def griewangk_function(x):
-		j = np.arange(1, len(x) + 1)
-		return np.sum(x**2 / 4000) - np.prod(np.cos(x / np.sqrt(j))) + 1
+		vec = x.vector
+		j = np.arange(1, len(vec) + 1)
+		return np.sum(vec**2 / 4000) - np.prod(np.cos(vec / np.sqrt(j))) + 1
 
 	bounds = np.array([[-400, 400] for _ in range(10)])
+	initial_pop = [Target(bounds) for _ in range(25)]
 
-	de = DE(griewangk_function, bounds, NP=25, max_iter=20000, F=0.5, CR=0.2)
+	de = DE(griewangk_function, bounds, initial_pop, max_iter=20000, F=0.5, CR=0.25)
 
 	average_fitness = np.inf
 	while average_fitness > 1e-5 and de.generation < de.max_iter:
@@ -60,6 +77,6 @@ if __name__ == '__main__':
 		average_fitness = np.mean([griewangk_function(x) for x in de.population])
 		print(f'Generation {de.generation}: {average_fitness}')
 
-	best_vector = de.population[de.get_best()]
-	print(f'Best vector: {best_vector}')
-	print(f'Best fitness: {griewangk_function(best_vector)}')
+	best_target = de.get_best()
+	print(f'Best vector: {best_target.vector}')
+	print(f'Best fitness: {griewangk_function(best_target)}')
