7a62fdce0da367c26e375abacb3e4af6c0fbf3ba
y8w2ng <80196333+y8w2ng@users.noreply.github.com>
2024-04-25 12:06:49 +0800

    Update michael.py
    "michael.py" "2nd commit" "change the approach from total area of worms/image size" to "distance of worms/imgage size"

--- michael.py
+++ michael.py
@@ -1,4 +1,4 @@
-# simple objective function for the optimise algorithm that penalises high worm density based on the approximity of worms
+'''# simple objective function for the optimise algorithm that penalises high worm density based on the approximity of worm centres
 def simplified_cost_worm_density(clew, image):
     """Calculate the cost of a clew of worms."""
     worm_density = 0
@@ -6,6 +6,19 @@ def simplified_cost_worm_density(clew, image):
         worm_density += worm.width * worm.r
     worm_density /= image.size
     return worm_density
+
+'''
+
+/ simple cost function that penalises high worm density based on distance of worms
+def simplified_cost_worm_density(clew, image):
+    """Calculate the cost of a clew of worms."""
+    worm_density = 0
+    for i in range(len(clew)):
+        for j in range(i+1, len(clew)):
+            worm_density += np.linalg.norm(clew[i].get_params()[:2] - clew[j].get_params()[:2])
+    worm_density /= image.size
+    return worm_density
+
 
 
 
