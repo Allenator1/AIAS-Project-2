a44b53e3b32137b7d409375991f9bd07e2592a41
y8w2ng <80196333+y8w2ng@users.noreply.github.com>
2024-04-25 12:30:15 +0800

    Update michael.py
    "michael.py" "4th commit" "new objective function based on the distance of the worms"

--- michael.py
+++ michael.py
@@ -1,3 +1,28 @@
+'''
+High level approach on worm density component
+
+To design an objective function that penalizes areas with too many overlapping worms, we will need to quantify the degree of overlap between worms in the generated image. One way to approach this is to calculate the density of worms in different regions of the image and penalize regions with high worm density.
+
+1. Define Worm Density Metric: Define a metric to quantify the density of worms in different regions of the image. This could be based on the number of worms overlapping in a particular region or the proximity of worms to each other.
+2. Preprocessing and Image Generation: Preprocess the original OCT retina image and generate a new image by overlaying camo worms, similar to the previous step.
+3. Objective Function: Design an objective function that evaluates the density of worms in the generated image and penalizes regions with high worm density. This function should take the new image with camo worms as input and return a scalar value representing the degree of penalization.
+4. Implementation: Implement the objective function in Python. This function should process the image with camo worms and calculate the worm density metric.
+5. Integration with DE Algorithm: Integrate the objective function with the DE algorithm by passing it as a parameter to the DE class constructor.
+'''
+# simple objective function to calculate the density of worms based on the distance between worm
+def simplified_cost_worm_density(clew, image):
+    """Calculate the cost of a clew of worms."""
+    worm_density = 0
+    for i in range(len(clew)):
+        for j in range(i+1, len(clew)):
+            worm_density += np.linalg.norm(clew[i].get_params()[:2] - clew[j].get_params()[:2]) #distance between worm centers by calculating the Euclidean distance between the centre-points of the worms
+    worm_density /= image.size
+    return worm_density
+
+
+
+
+#option 2
 '''# simple objective function for the optimise algorithm that penalises high worm density based on the approximity of worm centres
 def simplified_cost_worm_density(clew, image):
     """Calculate the cost of a clew of worms."""
@@ -6,18 +31,7 @@ def simplified_cost_worm_density(clew, image):
         worm_density += worm.width * worm.r
     worm_density /= image.size
     return worm_density
-
 '''
-
-# simple cost function that penalises high worm density based on distance of worms
-def simplified_cost_worm_density(clew, image):
-    """Calculate the cost of a clew of worms."""
-    worm_density = 0
-    for i in range(len(clew)):
-        for j in range(i+1, len(clew)):
-            worm_density += np.linalg.norm(clew[i].get_params()[:2] - clew[j].get_params()[:2])
-    worm_density /= image.size
-    return worm_density
 
 
 
