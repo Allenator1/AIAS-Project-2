6ae5e189bf0eb78f1cae975053b8e68b35290b9f
Allen <allenantony2001@gmail.com>
2024-04-30 20:01:29 +0800

    chore: testing..

--- optimize.py
+++ optimize.py
@@ -10,17 +10,16 @@ from cost_function.yi import *
 from Camo_Worm import Camo_Worm
 import util
 
-NUM_WORMS = 50
+POPULATION_SIZE = 10
 
 
-def final_cost(clew, var_map, image):
+def final_cost(worm, var_map, image):
     """
     Final cost function that combines the cost functions from Robbie and Allen.
     """
-    distance_cost = maximise_distances(clew)
-    variance_cost = maximise_global_variance(clew, var_map)
-    camo_cost = minimise_local_variance(clew, image)
-    return  3 * camo_cost + 1 * variance_cost + 2 * distance_cost
+    variance_cost = maximise_global_variance(worm, var_map)
+    # camo_cost = minimise_local_variance(worm, image)
+    return variance_cost
 
 
 if __name__ == '__main__':
@@ -31,36 +30,38 @@ if __name__ == '__main__':
     image = np.array(Image.open(image_path).convert('L'))
     image = np.flipud(image)
 
-    bounds = Clew.generate_bounds(NUM_WORMS, image.shape)
-    initial_population = [Clew(bounds) for _ in range(NUM_WORMS)]
+    height, width = image.shape
+    worm_bounds = (0, width, 0, height)
+    bounds = Camo_Worm.generate_bounds(worm_bounds)
+    initial_population = [Camo_Worm(bounds) for _ in range(POPULATION_SIZE)]
 
-    blurred_image = cv2.GaussianBlur(image, (5, 5), 0, borderType=cv2.BORDER_REPLICATE)
-    median_filter = cv2.medianBlur(image, 5)
-    var_map = cv2.Laplacian(blurred_image, cv2.CV_64F, borderType=cv2.BORDER_REPLICATE)
-    var_map = np.abs(var_map)
+    gaussian_img = cv2.GaussianBlur(image, (5, 5), 0, borderType=cv2.BORDER_REPLICATE)
+    median_img = cv2.medianBlur(image, 5)
+    var_map = np.abs(cv2.Laplacian(gaussian_img, cv2.CV_64F, borderType=cv2.BORDER_REPLICATE))
     var_map = cv2.normalize(var_map, None, 0, 1, cv2.NORM_MINMAX)
 
-    cost_fn = lambda x: final_cost(x, var_map, image)
+    cost_fn = lambda x: final_cost(x, var_map, median_img)
 
     # Create an instance of the DE algorithm
     de = DE(
         objective_function=cost_fn,
         bounds=bounds,
         initial_population=initial_population,
-        max_iter=100,        
-        F=0.1,
+        max_iter=1000,        
+        F=0.5,
         CR=0.9
     )
 
     # Run the DE algorithm
     while de.generation < de.max_iter:
         de.iterate()
-        print(f"Generation {de.generation}: Best cost = {cost_fn(de.get_best())}")
+        best_cost = cost_fn(de.get_best())
+        print(f"Generation {de.generation}: Best cost = {best_cost}")
 
-    # Get the best clew
-    best_clew = de.get_best()
+    # Get the best worm
+    best_worm = de.get_best()
 
-    # Visualize the best clew from the final population
+    # Visualize the best worm from the final population
     drawing = util.Drawing(image)
-    drawing.add_worms(best_clew.worms)
+    drawing.add_worms([best_worm])
     drawing.show()
