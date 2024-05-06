a9fdcfcf5e6e28fc7b16a8c2270dc788a393fb21
Allen <allenantony2001@gmail.com>
2024-05-06 23:59:59 +0800

    refactor

--- cost.py
+++ cost.py
@@ -24,18 +24,3 @@ def minimise_overlap(worm, mask):
     """
     overlap_cost = np.sum(mask[worm.indices])
     return overlap_cost ** 2
-
-
-def straighten_worm(worm):
-    """
-    Calculate the cost of a clew of worms based on the deviation from a straight line.
-    """
-    # Get the intermediate points along the worm's path
-    intermediate_points = worm.intermediate_points()
-
-    # Calculate the deviation from a straight line
-    straight_diff = abs(worm.approx_length() - np.linalg.norm(intermediate_points[-1] - intermediate_points[0]))
-    straight_diff = np.exp(straight_diff) - 1
-
-    # Combine the length difference and deviation into a single cost value
-    return straight_diff
