a9fd235d19ecf755385fc29a66e5f962c0f191b5
Allen <allenantony2001@gmail.com>
2024-05-08 15:13:05 +0800

    refactor: move anchor generating function to optimize_de.py

--- optimize_de.py
+++ optimize_de.py
@@ -5,7 +5,6 @@ import cv2
 
 from DE import DE
 from Camo_Worm import Camo_Worm
-from anchor import generate_boxes
 from tqdm import tqdm
 import util
 
@@ -113,6 +112,54 @@ def recursive_subdivision_optimisation(image, max_depth=4, debug=True):
 
     subdivision_worm(0, 0, im_height, im_width, max_depth)
     return worms
+
+
+NUM_ANCHORS_X = 4
+NUM_ANCHORS_Y = 12
+ASPECT_RATIOS = [1.5, 2]     # w/h
+ANCHOR_SCALE = 1
+NUM_SCALES = 1
+
+
+def generate_boxes(imshape):
+    """Generates multiscale anchor boxes. 
+    Code is adapted from the repository of EfficientDet: https://arxiv.org/abs/1911.09070"""
+    anchor_configs = []
+    for scale_octave in range(NUM_SCALES):
+        for aspect in ASPECT_RATIOS:
+            stride = (imshape[0] // NUM_ANCHORS_Y, imshape[1] // NUM_ANCHORS_X)
+            anchor_configs.append((stride, scale_octave / float(NUM_SCALES), aspect, ANCHOR_SCALE))
+
+    all_boxes = []
+
+    for config in anchor_configs:
+        stride, octave_scale, aspect, scale = config
+        base_anchor_size_x = scale * stride[1] * 2**octave_scale
+        base_anchor_size_y = scale * stride[0] * 2**octave_scale
+
+        aspect_x = np.sqrt(aspect)
+        aspect_y = 1.0 / aspect_x
+
+        anchor_size_x_2 = base_anchor_size_x * aspect_x / 2.0
+        anchor_size_y_2 = base_anchor_size_y * aspect_y / 2.0
+
+        x = np.arange(stride[1] / 2, imshape[1], stride[1])
+        y = np.arange(stride[0] / 2, imshape[0], stride[0])
+        xv, yv = np.meshgrid(x, y)
+        xv = xv.reshape(-1)
+        yv = yv.reshape(-1)
+
+        boxes = np.vstack((yv - anchor_size_y_2, xv - anchor_size_x_2,      # [y1, x1, y2, x2]
+                           yv + anchor_size_y_2, xv + anchor_size_x_2))
+        boxes = np.swapaxes(boxes, 0, 1)
+        all_boxes.append(boxes)
+    all_boxes = np.vstack(all_boxes)
+
+    lower_bounds = np.array([0, 0, 0, 0])
+    upper_bounds = np.array([imshape[0], imshape[1], imshape[0], imshape[1]])
+
+    all_boxes = np.clip(all_boxes, lower_bounds, upper_bounds)
+    return all_boxes
 
 
 def multiscale_optimisation(image):
